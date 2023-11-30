d513bd063549ab8a62a4b9f81b63b5697dbbcb95
davyken <davykennang552@gmail.com>
2023-11-30 13:44:26 +0100

    modified content

---
+++ index.js
@@ -0,0 +1,33 @@
+// question 1
+function minNumber(a, b) {
+  if (a < b) {
+    return a
+  } else if (a > b) {
+    return b
+  }
+}
+console.log(minNumber(5, 15))
+// question 2
+function maxNumber(c, d) {
+  if (c < d) {
+    return d
+  } else if (c > d) {
+    return c
+  }
+}
+console.log(maxNumber(50, 15))
+// question 3
+function sum(a, b) {
+  return a + b
+}
+console.log(sum(5, 25))
+// question 4
+function pow(a, b) {
+  let sum = 1
+  for (let i = 1; i <= b; i++) {
+    sum *= a
+  }
+  return (sum)
+}
+console.log(pow(5, 3))
+console.log(pow(6, 3))
